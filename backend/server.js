3b4fc6464c05b7e3bbfc328a8054bc2d8d2b1443
Aestheticsdata <scb_international@yahoo.com>
2023-04-23 17:05:49 +0200

    db sript

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,40 @@
-const cors = require('@fastify/cors');
+// const cors = require('@fastify/cors');
+//
+// const fastify = require('fastify')({
+//   logger: true,
+// });
+//
+// fastify.register(cors, {
+//   origin: 'http://localhost:3000',
+// });
+//
+// fastify.get('/hello', function (request, reply) {
+//   reply.send({ hello: 'hello world and BKMK' })
+// });
+//
+// fastify.listen({ port: 5000 }, function (err, address) {
+//   if (err) {
+//     fastify.log.error(err)
+//     process.exit(1)
+//   }
+// });
 
-const fastify = require('fastify')({
-  logger: true,
-});
 
-fastify.register(cors, {
-  origin: 'http://localhost:3000',
-});
+// Require the framework and instantiate it
+const fastify = require('fastify')({ logger: true });
 
-fastify.get('/hello', function (request, reply) {
-  reply.send({ hello: 'hello world and BKMK' })
-});
+// Declare a route
+fastify.get('/', async (request, reply) => {
+  return { hello: 'world' }
+})
 
-fastify.listen({ port: 5000 }, function (err, address) {
-  if (err) {
-    fastify.log.error(err)
-    process.exit(1)
+// Run the server!
+const start = async () => {
+  try {
+    await fastify.listen({ port: 3000 })
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
   }
-});
+}
+start();
---
+++ backend/src/db/bkmk.sql
@@ -0,0 +1,65 @@
+CREATE DATABASE bkmk;
+
+USE bkmk;
+
+CREATE TABLE user (
+    id           INT(11) AUTO_INCREMENT PRIMARY KEY,
+    name         VARCHAR(20) NOT NULL,
+    password     VARCHAR(60) NOT NULL,
+    email        VARCHAR(250) NOT NULL UNIQUE,
+    registerDate DATE NOT NULL
+);
+
+CREATE TABLE category (
+    id      INT(11) AUTO_INCREMENT PRIMARY KEY,
+    name    VARCHAR(20) NOT NULL UNIQUE,
+    color   VARCHAR(20) NOT NULL,
+    user_id INT(11),
+    FOREIGN KEY (user_id) REFERENCES user(id)
+);
+
+CREATE TABLE url (
+    id          INT(11) AUTO_INCREMENT PRIMARY KEY,
+    original    VARCHAR(2048) NOT NULL,
+    short       VARCHAR(255)
+);
+
+CREATE TABLE alarm (
+    id          INT(11) AUTO_INCREMENT PRIMARY KEY,
+    frequency   INT(3),
+    date_added  DATE NOT NULL
+);
+
+CREATE TABLE bookmark_group (
+    id      INT(11) AUTO_INCREMENT PRIMARY KEY,
+    user_id int(11),
+    title   VARCHAR(256),
+    color   VARCHAR(20),
+    FOREIGN KEY (user_id) REFERENCES user(id)
+);
+
+CREATE TABLE bookmark (
+    id              INT(11) AUTO_INCREMENT PRIMARY KEY,
+    url_id          INT(11),
+    user_id         INT(11),
+    group_id        INT(11),
+    alarm_id        INT(11),
+    title           VARCHAR(60) NOT NULL,
+    screenshot_path VARCHAR(512),
+    priority        INT(3),
+    notes           TEXT,
+    stars           INT(5),
+    date_added      DATE NOT NULL,
+    FOREIGN KEY (group_id)  REFERENCES bookmark_group(id),
+    FOREIGN KEY (url_id)    REFERENCES url(id),
+    FOREIGN KEY (user_id)   REFERENCES user(id),
+    FOREIGN KEY (alarm_id)  REFERENCES alarm(id)
+
+);
+
+CREATE TABLE bookmark_category (
+   bookmark_id INT(11),
+   category_id INT(11),
+   FOREIGN KEY (bookmark_id) REFERENCES bookmark(id),
+   FOREIGN KEY (category_id) REFERENCES category(id)
+);
