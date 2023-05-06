c75e535f86373d9cb547df1d44c95b4d194c3a84
Aestheticsdata <scb_international@yahoo.com>
2023-05-06 18:39:56 +0200

    css

--- frontend/src/components/shared/sharedLoginForm/sharedLoginForm.tsx
+++ frontend/src/components/shared/sharedLoginForm/sharedLoginForm.tsx
@@ -20,48 +20,49 @@ const SharedLoginForm = ({
       <div className="text-4xl font-thin">Bookmark Advanced</div>
 
       <form className="flex w-full flex-col items-center space-y-8" onSubmit={handleSubmit(onSubmit)}>
-        {displayEmailField ? (
-          <>
+        {displayEmailField && (
+          <div className="relative w-11/12 flex flex-col justify-center">
             <input
               type="email"
               placeholder="email"
-              className="w-11/12 border-b border-b-formsGlobalColor bg-transparent px-2 text-xl placeholder-grey2
+              className="w-full border-b border-b-formsGlobalColor bg-transparent px-2 text-xl placeholder-grey2
               outline-none focus:border-b-2"
               {...register("email", { required: true })}
             />
             {errors.email &&
-              <div>Email obligatoire</div>
+              <div className="flex text-red-500 justify-start">Email obligatoire</div>
             }
-          </>
-        ) : null}
-        {displayPasswordField ? (
-          <div className="relative w-11/12">
-            <input
-              type={showPassword ? "text" : "password"}
-              placeholder="password"
-              className="w-full border-b border-b-formsGlobalColor bg-transparent px-2 text-xl placeholder-grey2
-              outline-none focus:border-b-2"
-              {...register("password", { required: true })}
-            />
-            {errors.password &&
-              <div>password obligatoire</div>
-            }
-            <span
-              className="absolute right-2 bottom-1 cursor-pointer"
-              onClick={() => setShowPassword(!showPassword)}
-            >
-              {showPassword ? (
-                <FontAwesomeIcon icon={faEyeSlash} />
-              ) : (
-                <FontAwesomeIcon icon={faEye} />
-              )}
-            </span>
           </div>
-        ) : null}
+        )}
+        {displayPasswordField && (
+          <div className="relative w-11/12">
+            <div className="flex flex-row">
+              <input
+                type={showPassword ? "text" : "password"}
+                placeholder="password"
+                className="w-full border-b border-b-formsGlobalColor bg-transparent px-2 text-xl placeholder-grey2
+                outline-none focus:border-b-2"
+                {...register("password", { required: true })}
+              />
+              <div
+                className="bottom-1 cursor-pointer"
+                onClick={() => setShowPassword(!showPassword)}
+              >
+                {showPassword ? (
+                  <FontAwesomeIcon icon={faEyeSlash} />
+                ) : (
+                  <FontAwesomeIcon icon={faEye} />
+                )}
+              </div>
+            </div>
+            {errors.password &&
+              <div className="flex text-red-500 justify-start">password obligatoire</div>
+            }
+          </div>
+        )}
 
 
         <button
-          // type="submit"
           disabled={!isDirty || !isValid}
           className="h-8 w-11/12 rounded border border-formsGlobalColor bg-transparent bg-grey01alpha text-2xl
             font-medium uppercase text-formsGlobalColor transition-all hover:text-formsGlobalColorHover
