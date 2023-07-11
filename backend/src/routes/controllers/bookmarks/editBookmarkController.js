437525df00b26739bfe7583f7d7e742cc7b49d92
Aestheticsdata <scb_international@yahoo.com>
2023-07-11 21:54:45 +0200

    update bookmark title and url

--- backend/src/routes/controllers/bookmarks/editBookmarkController.js
+++ backend/src/routes/controllers/bookmarks/editBookmarkController.js
@@ -1,5 +1,47 @@
+const { format } = require('date-fns');
+const dbConnection = require('../../../db/dbinitmysql');
+const jimpHelper = require("./helpers/jimpHelper");
+
 module.exports = async (req, res) => {
   console.log("bookmark edit", req.body);
   console.log("bookmark edit title", req.body.title);
+
+  const conn = await dbConnection();
+
+  const sqlBookmark = `SELECT * FROM bookmark WHERE id="${req.body.id}"`;
+  let originalBookmark = null;
+  try {
+    const result = await conn.execute(sqlBookmark);
+    originalBookmark = result[0][0];
+  } catch (e) {
+    return res.status(500).json({ msg: "error getting bookmark : ", e });
+  }
+
+  // title
+  if (req.body.title !== originalBookmark.title) {
+    try {
+      await conn.execute(`UPDATE bookmark SET title="${req.body.title}" WHERE id="${originalBookmark.id}"`);
+    } catch (e) {
+      return res.status(500).json({ msg: "error updating title : ", e });
+    }
+  }
+
+  // url
+  let originalURL = null;
+  try {
+    const result = await conn.execute(`SELECT * FROM url WHERE id="${originalBookmark.url_id}"`);
+    originalURL = result[0][0];
+  } catch (e) {
+    return res.status(500).json({ msg: "error getting url : ", e });
+  }
+  if (req.body.url !== originalURL.original) {
+    try {
+      await conn.execute(`UPDATE url SET original="${req.body.url}" WHERE id="${originalURL.id}"`);
+    } catch (e) {
+      return res.status(500).json({ msg: "error updating url : ", e });
+    }
+  }
+
+  conn.end();
   return res.status(200).json({ msg: "bookmark edited" });
 }
